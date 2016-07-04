87d4915faa4f4a01f6cca6883809dfb102525767
bkil-syslogng <tamas.nagy@balabit.com>
2016-07-04 16:46:54 +0200

    debug print, fake new()

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,40 @@ struct TreeNode {
   right: Option<Box<TreeNode>>
 }
 
-fn get(t: &TreeNode, k: u32) -> Option<u32> {
-  if (k == t.key) {
-    Some(t.value)
-  } else {
-    let n = {
-      if (k < t.key) {
-        &t.left
-      } else {
-        &t.right
-      }
-    };
-    match n {
-      &None => None,
-      &Some(ref sn) => get(sn.as_ref(), k)
+impl TreeNode {
+  fn new() -> TreeNode {
+    TreeNode {
+      key: 0,
+      value: 0,
+      left: None,
+      right: None
     }
   }
-}
 
-fn set(t: &mut TreeNode, k: u32, v: u32) {
+  fn get(self: &TreeNode, k: u32) -> Option<u32> {
+    if (k == self.key) {
+      Some(self.value)
+    } else {
+      let n = {
+        if (k < self.key) {
+          &self.left
+        } else {
+          &self.right
+        }
+      };
+      match n {
+        &None => None,
+        &Some(ref sn) => sn.as_ref().get(k)
+      }
+    }
+  }
+
+  fn set(self: &mut TreeNode, k: u32, v: u32) {
+  }
 }
 
 #[test]
 fn it_works() {
+  let t = TreeNode::new();
+  println!("{:?}", t)
 }
