f2b96b9007d1f341c734be531c55dd123f1c4ffd
bkil-syslogng <tamas.nagy@balabit.com>
2016-07-04 18:34:13 +0200

    WIP boxing experiment

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,58 @@
 #[derive(Debug)]
-struct TreeNode {
-  key: Option<u32>,
-  value: u32,
-  left: Option<Box<TreeNode>>,
-  right: Option<Box<TreeNode>>
+enum Tree {
+  Empty,
+  Node(Box<TreeNode>)
 }
 
-impl TreeNode {
-  fn new() -> TreeNode {
-    TreeNode {
-      key: None,
-      value: 0,
-      left: None,
-      right: None
-    }
+#[derive(Debug)]
+struct TreeNode {
+  key: u32,
+  value: u32,
+  left: Tree,
+  right: Tree
+}
+
+impl Tree {
+  fn new() -> Tree {
+    Tree::Empty
   }
 
-  fn get(self: &TreeNode, k: u32) -> Option<u32> {
-    match self.key {
-      None => None,
-      Some(sk) =>
-        if (k == sk) {
-          Some(self.value)
+  fn get(self: &Tree, k: u32) -> Option<u32> {
+    match self {
+      &Tree::Empty => None,
+      &Tree::Node(ref nt) => {
+        let t = nt.as_ref();
+        if (k == t.key) {
+          Some(t.value)
         } else {
           let n = {
-            if (k < sk) {
-              &self.left
+            if (k < t.key) {
+              &t.left
             } else {
-              &self.right
+              &t.right
             }
           };
-          match n {
-            &None => None,
-            &Some(ref sn) => sn.as_ref().get(k)
-          }
+          n.get(k)
         }
+      }
     }
   }
 
 //  fn set_helper(t: &TreeNode) -> Option<Box<TreeNode>> {
 //  }
 
-  fn set(self: &mut TreeNode, k: u32, v: u32) {
-    match self.key {
-      None => {
-        self.key = Some(k);
-        self.value = v
-      }
-      Some(sk) => {
-        if (k == sk) {
-          self.value = v
+  fn set(self: &mut Tree, k: u32, v: u32) {
+    match self {
+      &mut Tree::Empty => {
+        let mut n = TreeNode {key: k, value: v, left: Tree::Empty, right: Tree::Empty};
+        self = Tree::Node(Box::new(n));
+      },
+      &mut Tree::Node(ref nt) => {
+        let t = nt.as_ref();
+        if (k == t.key) {
+          t.value = v
         } else {
-          match self.left {
-            None => {
-              let new = TreeNode {key: Some(k), value: v, left: None, right: None};
-              self.left = Some(Box::new(new));
-            },
-            Some(&mut ref sn) => sn.as_ref().set(k, v)
-          };
+          t.left.set(k, v)
         }
       }
     }
@@ -66,13 +61,13 @@ impl TreeNode {
 
 #[test]
 fn new_empty() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   assert_eq!(t.get(0), None);
 }
 
 #[test]
 fn set_get_missing() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   t.set(1,2);
   let v = t.get(3);
   assert_eq!(v, None);
@@ -80,7 +75,7 @@ fn set_get_missing() {
 
 #[test]
 fn set_get_1() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   t.set(1,41);
   let v = t.get(1);
   println!("{:?}", v);
@@ -89,7 +84,7 @@ fn set_get_1() {
 
 #[test]
 fn set_get_1_1() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   t.set(1,42);
   t.set(1,41);
   assert_eq!(t.get(1).unwrap(), 41);
@@ -97,7 +92,7 @@ fn set_get_1_1() {
 
 #[test]
 fn set_get_1_2() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   t.set(1,41);
   t.set(2,42);
   assert_eq!(t.get(1).unwrap(), 41);
@@ -106,7 +101,7 @@ fn set_get_1_2() {
 
 #[test]
 fn set_get_1_3_2() {
-  let mut t = TreeNode::new();
+  let mut t = Tree::new();
   t.set(1,41);
   t.set(3,43);
   t.set(2,42);
